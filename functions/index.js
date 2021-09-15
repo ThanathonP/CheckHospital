d1dea00a52512b293753d6d26ceb64052759ef81
pond1597 <6106021622101@fitm.kmutnb.ac.th>
2021-09-15 17:15:29 +0700

    n1

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,47 @@
 const functions = require("firebase-functions");
+const request = require("request-promise");
+const config = require("./config.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    //functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
-});
-exports.webhook = functions.https.onRequest((request, response) => {
-    //functions.logger.info("Hello logs!", {structuredData: true aaaa});
-    response.send("Hello from webhook");
-});
+//ตั้งค่า region และปรับ timeout และเพิ่ม memory
+const region = "asia-east2";
+const runtimeOptions = {
+    timeoutSeconds: 4,
+    memory: "2GB"
+};
+
+//ทำ webhook request url
+exports.webhook = functions
+    .region(region)
+    .runWith(runtimeOptions)
+    .https.onRequest(async(req, res) => {
+        console.log("LINE REQUEST BODY", JSON.stringify(req.body));
+
+        //[0] ดึงข้อมูลจาก request message ที่มาจาก LINE
+        const replyToken = req.body.events[0].replyToken;
+        const messages = [{
+                "type": "text ",
+                "text ": req.body.events[0].message.text //ดึง message ที่ส่งเข้ามา
+            },
+            {
+                "type ": "text ",
+                "text ": JSON.stringify(req.body) //ลองให้พ่น สิ่งที่่ LINE ส่งมาให้ทั้งหมดออกมาดู
+            }
+        ];
+
+        //ยิงข้อความกลับไปหา LINE (ส่ง response กลับไปหา LINE)
+        return lineReply(replyToken, messages);
+    });
+
+//function สำหรับ reply กลับไปหา LINE โดยต้องการ reply token และ messages (array)
+const lineReply = (replyToken, messages) => {
+    const body = {
+        replyToken: replyToken,
+        messages: messages
+    };
+    return request({
+        method: "POST",
+        uri: `${config.lineMessagingApi}/reply`,
+        headers: config.lineHeaders,
+        body: JSON.stringify(body)
+    });
+};
---
+++ functions/pond.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="index.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <h1>ponddddddfknmgkjlfdghlkdfjg;า่สเอด้สาืดเ้สาื้ดกเldfg</h1>
+</body>
+</html>
